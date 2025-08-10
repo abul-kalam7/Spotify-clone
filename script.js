19613b672f3624564d61e396568af913420c0432
Abul Kalam <kvai2077@gmail.com>
2025-08-11 00:12:46 +0600

    day4 1:48

--- script.js
+++ script.js
@@ -20,6 +20,18 @@ async function getSongs() {
 async function main() {
     let songs = await getSongs();
     console.log(songs);
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + song;
+    }
     
+    //Play the first song
+    var audio = new Audio(songs[0])
+    //audio.play();
+
+    audio.addEventListener("loadeddata", () => {
+        console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    })
 }
 main()
