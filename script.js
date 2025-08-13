1706021a01e8c2a64a6651e7562a4d079e88f4d4
Abul Kalam <kvai2077@gmail.com>
2025-08-13 16:37:43 +0600

    1:51

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function getSongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
+            songs.push(element.href.split("/songs/")[1])
         }
     }
     return songs;
@@ -23,7 +23,7 @@ async function main() {
 
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songUL.innerHTML = songUL.innerHTML + song;
+        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li>`;
     }
     
     //Play the first song
